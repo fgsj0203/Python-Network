2de3b58ed2b4f94b9e152bf7812ccc775b689c5f
Francisco Gomes <fgsj.dev@outlook.com>
2022-02-08 16:28:13 -0300

    Code organized with comments v1.1

--- Tools Python Security Beginner/GeneratorPassword.py
+++ Tools Python Security Beginner/GeneratorPassword.py
@@ -1,13 +1,24 @@
+"""
+Author = Francisco Gomes
+Date = 08/02/2022
+Project = Create generator password strong with best practices of CyberSecurity
+"""
+
+# Importing libraries used in project
 import random, string
 
-sizePassword = 16  # Best practice for size password
 
-#Method ascii_letters using lower case and Upper Case
-#String digits with numbers between 0 and 9
+# Variable of size password for best practices cybersecurity
+sizePassword = 16
+
+# Method ascii_letters using lower case and Upper Case
+# String digits with numbers between 0 and 9
+# Concatenated letters, numbers and character special
 chars = string.ascii_letters + string.digits + "!@#$%&*()-=+,.;?][ç" # Receiving structure of password generate
 
-passwordRandom = random.SystemRandom() # function SystemRandom using other library (OS) urandom
+passwordRandom = random.SystemRandom() # function SystemRandom using other library (OS) "urandom"
 
+# Printing password generated with parameters
 print("".join(passwordRandom.choice(chars) for i in range(sizePassword)))
 
 
